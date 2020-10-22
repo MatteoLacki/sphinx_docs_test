1a7acc856036fa728c8367b2e603554a782d541c
matteo <matteo.lacki@gmail.com>
2020-10-22 15:21:52 +0200

    redoing stuff knowing how :)

---
+++ sphinx_docs_test/test.py
@@ -0,0 +1,9 @@
+def foo(a, b, c):
+    """This function kills communists.
+
+    Args:
+        a (int): argument for killing
+        b (float): argument against killing
+        c (boolean): execute?
+    """
+    pass
